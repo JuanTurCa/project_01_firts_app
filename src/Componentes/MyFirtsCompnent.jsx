c5842f550c87358407f8d15682e7752f128c651b
JMTC <2420172030@estudiatesunibague.edu.co>
2024-06-27 08:01:04 -0500

    I create my firts React

---
+++ src/Componentes/MyFirtsCompnent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MyFirtsCompnent = () => {
+    return (
+        <>
+            <h1>My First Component</h1>
+            <p>This is my first component in React</p>
+        </>
+    )
+}
+
